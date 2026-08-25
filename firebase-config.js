5555fc0817a9a1503f844db8304216bf9365601f
miniseung-web <miniseung@dongdaemun.ms.kr>
2026-08-26 07:00:10 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -8,10 +8,6 @@ export const firebaseConfig = {
   measurementId: "G-SWG6765ZVY"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 // Firebase Authentication에서 아래 이메일 계정을 1개 만들고,
 // 비밀번호를 교사가 사용할 관리자 번호(6자리 이상 숫자 권장)로 설정하세요.
 export const ADMIN_EMAIL = "teacher@rights-map.local";
